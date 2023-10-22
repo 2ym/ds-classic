fffde6c6a4ffa0d0ed4b8182bc0414c561a2067c
2ym <78376740+2ym@users.noreply.github.com>
2023-10-22 13:59:19 +0200

    -

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/pcag_logo.png',
         },
         items: [
           {
